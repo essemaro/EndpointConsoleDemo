316443108b22695a176bc5b94eae04d7d36496fe
Robertson <srobertson@midatlanticcorporate.org>
2021-04-19 10:15:33 -0400

    Add project files.

---
+++ EndpointConsoleDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EndpointConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
